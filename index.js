ce84c136b0c85cf732a9aaa101833dcb6f4c2053
SaadHere3448 <Bosssaad545@gmail.com>
2025-08-05 02:49:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -46,3 +46,32 @@ function revealOnScroll() {
 window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll); // reveal immediately on page load if visible
 
+let navies = document.querySelectorAll('.nav-items')
+let document_width = Math.max(
+    document.body.clientWidth,
+    document.body.scrollWidth
+)
+let document_height = Math.max(
+    document.body.scrollHeight,
+    document.body.clientHeight
+)
+// let log = console.log
+let nav = document.getElementById('nav-left')
+
+if (document_width <= 900 || document_height <= 1200) {
+    navies.forEach(navy => {
+        navy.remove()
+    })
+    let menu = document.createElement('img')
+    menu.src = 'https://up.yimg.com/ib/th/id/OIP.MO13Yb2PjehyQD7s6Lsx_gHaHa?pid=Api&rs=1&c=1&qlt=95&w=121&h=121' // Added Menu Icon Image Here
+    menu.className = 'menu-img'
+    nav.append(menu)
+    menu.addEventListener('click', function(){
+        let menu_bar = document.getElementById('menu-bar')
+        if (menu_bar.classList.contains('hidden')){
+            menu_bar.classList.remove('hidden')
+        }else{
+            menu_bar.classList.add('hidden')
+        }
+    })
+}
